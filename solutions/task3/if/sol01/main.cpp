8b97628d3a62021f24a1fc3e5d301b3609d2555e
Pavel Kasila <pavel.kasila@gmail.com>
2022-02-12 23:15:16 +0300

    Add solutions of Task 3 by IF

--- solutions/task3/if/sol01/main.cpp
+++ solutions/task3/if/sol01/main.cpp
@@ -1,6 +1,33 @@
+// медленное решение
+
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    uint64_t divider = 1e9 + 7;
+
+    int questions_count;
+    cin >> questions_count;
+
+    for (int question = 0; question < questions_count; question++) {
+        int n, p;
+        cin >> n >> p;
+
+        uint64_t counter = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i ^ j) & (1ll << (p - 1))) {
+                    counter++;
+                }
+            }
+        }
+
+        counter %= divider;
+
+        cout << counter << endl;
+    }
     return 0;
 }
