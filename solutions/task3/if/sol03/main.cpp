fde6f3f7dbf2f419f8f0401e54eab8a271e17858
Pavel Kasila <pavel.kasila@gmail.com>
2022-02-13 20:43:27 +0300

    Task 3 solution 3

--- solutions/task3/if/sol03/main.cpp
+++ solutions/task3/if/sol03/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    int q;
+    cin >> q;
+    for(int i = 0;i < q;i++)
+    {
+        unsigned long long n,s = 0;
+        int p;
+        cin >> n >> p;
+        for(unsigned long long j = 0;j < n;j++)
+        {
+            for(unsigned long long k = j + 1;k < n;k++)
+            {
+                if(((j >> (p - 1)) & 1) != ((k >> (p - 1)) & 1))
+                    s++;
+            }
+        }
+        cout << s*2 << endl;
+    }
     return 0;
 }
