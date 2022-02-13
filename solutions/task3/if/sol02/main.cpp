efbc706e322bb6c8d856cfa2ab4f3462be036a15
Pavel Kasila <pavel.kasila@gmail.com>
2022-02-13 16:06:20 +0300

    Add solution 3 (slow with bitset)

--- solutions/task3/if/sol02/main.cpp
+++ solutions/task3/if/sol02/main.cpp
@@ -1,6 +1,24 @@
+// медленное решение (bitset)
+
 #include <iostream>
+#include <bitset>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int q, p;
+    long long n;
+    cin >> q;
+    while (q--) {
+        long long t = 0;
+        cin >> n >> p;
+        for (long long i = 0; i < n; i++) {
+            for (long long j = 0; j < n; j++) {
+                bitset<64> b(i ^ j);
+                if (b.test(p - 1))
+                    t++;
+            }
+        }
+        cout << t % 1000000007 << endl;
+    }
 }
