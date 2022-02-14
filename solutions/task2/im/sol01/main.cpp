8ff2b725cd9d24e554b8b428aec8b2f57ff514e5
Kostya Filipovich <44550668+ZFreeX@users.noreply.github.com>
2022-02-14 18:35:08 +0300

    Create main.cpp

--- solutions/task2/im/sol01/main.cpp
+++ solutions/task2/im/sol01/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <bitset>
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+long long n, k;
+cin >> n >> k;
+n--;
+bitset<64> b(n);
+int h = b.count();
+h %= k;
+cout << h;
+
 }
