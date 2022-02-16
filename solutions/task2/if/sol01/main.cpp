29090e83e45f93c970a8e8f9d2d5b200a9a48417
Pavel Kasila <pavel.kasila@gmail.com>
2022-02-16 08:00:00 +0300

    Add task 2 by IF (Khmel)

--- solutions/task2/if/sol01/main.cpp
+++ solutions/task2/if/sol01/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <fstream>
+#include <string>
+#include <bitset>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    int n;
+    fin >> n;
+    fin.ignore();
+    string s;
+    getline(fin, s);
+    bitset<32> b(n);
+    int key = b.count();
+    for (int i = 0; i < s.size(); i++) {
+        s[i] -= key;
+    }
+    fout << s;
     return 0;
 }
