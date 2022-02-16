91a62524b6848048312d148ed0397d3d52d7c12a
Pavel Kasila <pavel.kasila@gmail.com>
2022-02-16 08:00:00 +0300

    Extract needed from archive, remove archive

--- solutions/task2/im/sol01/main.cpp
+++ solutions/task2/im/sol01/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long long n, k;
+    cin >> n >> k;
+    vector<int> seq;
+    seq.push_back(0);
+    while (seq.size() < n) {
+        vector<int> curr = seq;
+        for (int i = 0; i < curr.size(); i++) {
+            curr[i]++;
+        }
+        seq.insert(seq.end(), curr.begin(), curr.end());
+    }
+    cout << seq[n - 1] % k;
     return 0;
 }
--- solutions/task2/im/sol02/main.cpp
+++ solutions/task2/im/sol02/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+using namespace std;
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+    long long n, k;
+
+    cin >> n >> k;
+
+    string x = "0";
+
+    while (x.length() < n) {
+
+
+        string s = x;
+        for (auto &e: s) {
+            e++;
+        }
+        x += s;
+
+    }
+    cout << (x[n - 1] - '0') % k;
+
+
 }
